926867a414c1125abefa38161d82d71756f591a2
Michael Sheets <git@whitefalls.org>
2014-06-18 03:57:19 -0500

    Point to ruby 1.8 shim
    Using a shim allows us to catch when 1.8 of ruby is not present and provide other options.
    
    #ignore

--- Support/arg_separators.rb
+++ Support/arg_separators.rb
@@ -1,4 +1,4 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby -wKU
+#!/usr/bin/env ruby18 -wKU
 
 commands = `/usr/local/bin/cmake --help-command-list`.to_a
 commands.shift # Skip version number
